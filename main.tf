55c049754decd4f306e777e0b93dfe3b6d597a99
alex2025W <alexey.tulaev@gmail.com>
2023-03-26 18:39:09 +0300

    added finish

--- main.tf
+++ main.tf
@@ -5,52 +5,127 @@ terraform {
       version = "0.61.0"
     }
   }
-  backend "s3" {
-    endpoint   = "storage.yandexcloud.net"
-    bucket     = "tf-state-bucket-mentor"
-    region     = "ru-central1-a"
-    key        = "issue1/lemp.tfstate"
-    access_key = "<access_key>"
-    secret_key = "<secret_key>"
-
-    skip_region_validation      = true
-    skip_credentials_validation = true
-  }
 }
 provider "yandex" {
-  token     = "<token>"
-  cloud_id  = "<cloud_id>"
-  folder_id = "<folder_id>"
-  zone      = "ru-central1-a"
+  token     = "y0_AgAAAABh8xp3AATuwQAAAADfcKhDH3Im-BLLRUqPTv3c4b7EcHnL_Dc"
+  cloud_id  = "b1g3511gbb5i7rp6vflk"
+  folder_id = "b1g56afs20h7lmp67brb"
+  zone      = "ru-central1-b"
 }
 
-resource "yandex_vpc_network" "network" {
-  name = "network"
+# Настройки провайдера
+
+resource "yandex_compute_instance" "vm-1" {
+  name = "terraform1"
+
+  resources {
+    cores  = 2
+    memory = 2
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8gk0mbbkpvmhp4psfr"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    user-data = "${file("meta_nginx")}"
+  }
 }
 
-resource "yandex_vpc_subnet" "subnet1" {
+resource "yandex_compute_instance" "vm-2" {
+  name = "terraform2"
+
+  resources {
+    cores  = 2
+    memory = 2
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd83hhoi03ga5bsau95m"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    user-data = "${file("meta_apache")}"
+  }
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
   name           = "subnet1"
-  zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.network.id
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.network-1.id
   v4_cidr_blocks = ["192.168.10.0/24"]
 }
 
-resource "yandex_vpc_subnet" "subnet2" {
-  name           = "subnet2"
-  zone           = "ru-central1-b"
-  network_id     = yandex_vpc_network.network.id
-  v4_cidr_blocks = ["192.168.11.0/24"]
+resource "yandex_lb_network_load_balancer" "foo" {
+  name = "terrafom-test-lb"
+  listener {
+    name = "my-listener"
+    port = 80
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+  attached_target_group {
+    target_group_id = yandex_lb_target_group.foo.id
+    healthcheck {
+      name = "http"
+      http_options {
+        port = 80
+        path = "/"
+      }
+    }
+  }
+}
+
+resource "yandex_lb_target_group" "foo" {
+  name      = "terraform-test"
+  region_id = "ru-central1"
+
+  target {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    address   = yandex_compute_instance.vm-1.network_interface.0.ip_address
+  }
+
+  target {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    address   = yandex_compute_instance.vm-2.network_interface.0.ip_address
+  }
+
+}
+
+output "internal_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+}
+
+output "internal_ip_address_vm_2" {
+  value = yandex_compute_instance.vm-2.network_interface.0.ip_address
 }
 
 
-module "ya_instance_1" {
-  source                = "./modules/instance"
-  instance_family_image = "lemp"
-  vpc_subnet_id         = yandex_vpc_subnet.subnet1.id
+output "external_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
 }
 
-module "ya_instance_2" {
-  source                = "./modules/instance"
-  instance_family_image = "lamp"
-  vpc_subnet_id         = yandex_vpc_subnet.subnet2.id
+output "external_ip_address_vm_2" {
+  value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
 }
